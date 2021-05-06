aa334856db3e2c9ad2cd8458657998732c97927b
Tosi123 <alrod1009@naver.com>
2021-05-06 16:48:30 +0900

    main 파일 추가

--- skel_project/bin/control.sh
+++ skel_project/bin/control.sh
@@ -4,6 +4,7 @@ cd ..
 PWD=`pwd`
 LANG=ko_KR.utf8
 PROCESS_NAME=`basename ${PWD}`
-PYTHON_PATH="/usr/local/bin/python3.6"
+PYTHON_PATH="python"
+#PYTHON_PATH="/usr/local/bin/python3.6"
 
 ${PYTHON_PATH} main.py $@ 2>> ./logs/${PROCESS_NAME}.err 
